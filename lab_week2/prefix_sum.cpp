1a75f4fb86885ecbed265d88cc912a901c52ba5a
Haris Mahmood <haris.mahmood@uconn.edu>
2023-02-07 23:27:44 -0500

    Update prefix_sum.cpp

--- lab_week2/prefix_sum.cpp
+++ lab_week2/prefix_sum.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 #include "prefix_sum.h"
+#include <iostream>
+#include <algorithm>
+#include <random>
+#include <vector>
+
+
+using namespace std;
 
 int prefix_sum(int array[], int last) { /* last must be less than length of the array */
   int sum = 0;
@@ -8,8 +15,6 @@ int prefix_sum(int array[], int last) { /* last must be less than length of the 
   }
   return sum;
 }
-
-
 
 
 bool non_negative_prefix_sum(int array[], int length) {
@@ -21,5 +26,51 @@ bool non_negative_prefix_sum(int array[], int length) {
       break;
     }
   }
-  return is_non_negtive;
+  return is_non_negative;
+}
+
+bool status_seq(vector<int> &spc) {
+    int add_ = 0;
+    int a;
+    for (a = 0; a < spc.size(); a++) {
+        add_ = add_ + spc[a];
+        if (0 > add_) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int input_;
+    cout << "Enter an n value:";
+    cin >> input_;
+    int rf = 2*input_;
+    vector<int> spc(rf);
+    int j;
+    for (j = 0; j < input_; j++) {
+        spc[j] = 1;
+    }
+    int k;
+    for (k = input_; k < rf; k++) {
+        spc[k] = -1;
+    }
+
+    random_device gn;
+    mt19937 g(gn());
+    int num_ = 0;
+    int all_ = 0;
+    do {
+        shuffle(spc.begin(), spc.end(), g);
+        all_++;
+        if (status_seq(spc)) {
+            num_++;
+        }
+    } 
+    
+    while (next_permutation(spc.begin(), spc.end()));
+
+    cout << "Total = " << all_ << endl;
+    cout << "Correct = " << num_ << endl;
+    return 0;
 }
