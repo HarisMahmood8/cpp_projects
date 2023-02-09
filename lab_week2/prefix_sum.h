1a740f958a70fd395d1db14295870cdef0d215e0
Haris Mahmood <haris.mahmood@uconn.edu>
2023-02-08 22:51:51 -0500

    Update prefix_sum.h

--- lab_week2/prefix_sum.h
+++ lab_week2/prefix_sum.h
@@ -1,6 +1,8 @@
 #ifndef __PREFIX_SUM_H
 #define __PREFIX_SUM_H
-
+#include <algorithm>
+#include <random>
+#include <vector>
 
 int prefix_sum(int array[], int last); 
 bool non_negative_prefix_sum(int array[], int length);
