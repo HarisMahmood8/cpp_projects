b5d0a8dc4a43633924a2f8bb8da28421df683092
Haris Mahmood <haris.mahmood@uconn.edu>
2023-04-14 04:04:29 -0400

    Update fractions.h

--- classwork_week11/fractions.h
+++ classwork_week11/fractions.h
@@ -1,4 +1,3 @@
-
 #include <vector>
 #include "bigint/Big Integer Library in Single file/bigint.h"
 #include <utility>
@@ -16,30 +15,32 @@ struct division {
   division() : cur(make_pair(to_bigint(1), to_bigint(1))) {};
   division(bigint tp, bigint btm) : cur(make_pair(tp, btm)) {};
   
-  void print() {
+ 
+
+  division operator+(division sec) {
+    bigint dvs = big_gcd(cur.second, sec.cur.second);
+
+    bigint btm = (cur.second * sec.cur.second) / dvs;
+    bigint tp = (cur.first * (btm / cur.second)) + (sec.cur.first * (btm / sec.cur.second));
+    
+    division ret(tp, btm);
+    return ret;
+  }
+
+   void outpt() {
     cout << cur.first << "/" << cur.second << endl;
   }
-
-  division operator+(division other) {
-    bigint dvs = big_gcd(cur.second, other.cur.second);
-
-    bigint btm = (cur.second * other.cur.second) / dvs;
-    bigint tp = (cur.first * (btm / cur.second)) + (other.cur.first * (btm / other.cur.second));
-    
-    division result(tp, btm);
-    return result;
-  }
   
-  division operator*(division other) {
-    division result((cur.first * other.cur.first),(cur.second * other.cur.second));
-    return result;
+  division operator*(division sec) {
+    division ret((cur.first * sec.cur.first), (cur.second * sec.cur.second));
+    return ret;
   }
 };
 
 struct Matrices_division {
   vector<vector<struct division>> mtrx;
 
-  void print() {
+  void outpt() {
     for(auto i = mtrx.begin(); i <= mtrx.end(); i++) {
       
     }
@@ -47,21 +48,22 @@ struct Matrices_division {
 
   Matrices_division(vector<vector<struct division>> mtrx) : mtrx(mtrx) {};
 
-  Matrices_division operator*(struct Matrices_division other) {
-    vector<vector<struct division>> result;
+  Matrices_division operator*(struct Matrices_division sec) {
+    vector<vector<struct division>> ret;
     for (int i = 0; i < mtrx.size(); i++) {
-      vector<struct division> sub_result;
+      vector<struct division> cur_rsl;
       for (int x = 0; x < mtrx[i].size(); x++) {
         division div;
-        for (int j = 0; j < other.mtrx.size(); j++) {
-          div = div + (mtrx[i][x] * other.mtrx[j][x]);
+        for (int j = 0; j < sec.mtrx.size(); j++) {
+          div = div + (mtrx[i][x] * sec.mtrx[j][x]);
         }
-        sub_result.push_back(div);
+        cur_rsl.push_back(div);
       }
-      result.push_back(sub_result);
+      ret.push_back(cur_rsl);
     }
-    return Matrices_division(result);
+    return Matrices_division(ret);
   }
 };
 
 #endif
+
